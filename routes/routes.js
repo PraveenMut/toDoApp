873188d3c55ede05b3692373c57cf2199ae05c3c
praveen <contact@praveenmuthu.dev>
2019-06-18 16:06:10 +1000

    had skeletonal structure to routes

---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+// require initial dependencies
+const express = require('express');
+const router = express.Router();
+
+// require model schema
+
+const Tasks = require('../models/tasks')
+
+// get all tasks
+
+// get single tasks
+
+// create task
+
+// update task
+
+// delete task
+
