af194d109d6da2e42ae10ac824a3eef7bb091659
praveen <contact@praveenmuthu.dev>
2019-06-19 12:14:43 +1000

    add initial html structure

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PORT = 3000;
 
 // use middleware dependencies
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(express.static());
 
 // use encoded form depdency
 app.use(express.urlencoded({extended: false}));
